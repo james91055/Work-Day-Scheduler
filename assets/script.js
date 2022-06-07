512ff7594820eceeaa5c474f1cb9664701729fcb
James <james@macbook-air.lan>
2022-06-07 00:32:38 -0700

    update time block

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,32 @@
-var now = moment().format("MMM Do, YYYY, h:mm:ss");
-$("#currentDay").text(now);
+var currentDate = $("#currentDay");
+// Track hours
+var timeNow = moment().hour();
+
+// Realtime update
+setInterval(rightNow, 1000);
+function rightNow() {
+  var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+  currentDate.text(now);
+}
+
+console.log($(".time-block"));
+console.log(timeNow);
+
+// Check Time blocks
+$(".row").each(function () {
+  var blockTime = parseInt($(this).attr("id"));
+
+  if (blockTime < timeNow) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+  } else if (blockTime === timeNow) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+    $(this).removeClass("future");
+  } else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+});
